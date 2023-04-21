d8b7925ba4ffc1f3c6e09d895dd1cc8a9f8a4840
MonkeyDNaruto <nischalshrestha324@gmail.com>
2023-04-21 09:58:16 +0545

    BMI Calculator

--- calculator.js
+++ calculator.js
@@ -15,6 +15,17 @@ app.post('/', (req, res) => {
     res.send("The total sum of number is " + total)
 })
 
+app.get("/bmicalculator", (req, res) => {
+    res.sendFile(__dirname + "/bmiCalculator.html")
+})
+
+app.post("/bmicalculator", (req, res) => {
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+    var bmi = weight / (height * height);
+    res.send("Calculated bmi is " + bmi);
+})
+
 app.listen(3000, (req, res) => {
     console.log("Server is listening on port 3000")
 })
